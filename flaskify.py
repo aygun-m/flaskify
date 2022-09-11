a4782e12deeff7b775ce0424ef4081f19914f0e2
felsef <felsef@protonmail.com>
2022-09-11 20:39:26 +0100

    complete

--- flaskify.py
+++ flaskify.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-
-from operator import index
-from readline import append_history_file
 from sys import argv
 import os
-from urllib.parse import ParseResultBytes
 version = '1.0'
 whatisthis = 'Flaskify can generate a flask application based on given criteria.'
 args = argv
@@ -21,10 +17,6 @@ Usage: flaskify.py <directory> -n=<app name> [options]"""
 -s='<SSName>'
 -t='<TPName>'
 """
-
-
-
-
 def checkArgs(args):
     if len(args) < 1:print(help)
     else:
@@ -62,7 +54,7 @@ def checkArgs(args):
                 if ' ' in webapp_name: print("Flask application name cannot include spaces")
                 else:
                     main_name = 'main.py'
-                    init_name = '__init__.py'
+                    init_name = '__init__.py' 
                     views_name = 'views.py'
                     static_directory_name = 'static/'
                     stylesheet_name = 'styles.css'
@@ -79,10 +71,15 @@ def checkArgs(args):
                             #flaskify/styles.css 1
                         #flaskify/templates 1
                             #flaskify/index.html 1
-                    main_file = f"""
-                    from {webapp_name} import app
-                    app.run(debug=True)
-                    """
+                    main_file = f"""from {webapp_name} import app
+app.run(debug=True)"""
+                    views_file = f"""from flask import Blueprint, render_template, request, session, url_for
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def index():
+    return render_template('index.html')"""
+                    #Blueprints handling
                     if blueprints == []:
                         init_file = f"""from flask import Flask
 from .views import views
@@ -117,7 +114,7 @@ app.config['SECRET_KEY] = 'mysecretkey'"""
                             loop_RG.append(', url_prefix=\'/\')')
                             for y in loop_RG:
                                 string += y
-                                string += ' '
+                                string += ''
                             forLoopRegister.append(string)
                             for x in forLoopRegister:
                                 init_file_reg += x
@@ -131,14 +128,14 @@ app.register_blueprint(views, url_prefix='/')
 app.config['SECRET_KEY'] = 'mysecretkey'"""
                     
                    #Init File done
-                        
+
                         
 
                     mk_webapp_name = os.path.join(dir, webapp_name)
                     os.mkdir(mk_webapp_name)
                     print(f"Created directory at: {mk_webapp_name}")
                     mk_main_name = os.path.join(mk_webapp_name, main_name)
-                    with open(mk_main_name, 'w') as f:f.write("This is main.py")
+                    with open(mk_main_name, 'w') as f:f.write(main_file)
                     print(f"Created file at: {mk_main_name}")
                     mk_app_name = os.path.join(mk_webapp_name, webapp_name)
                     os.mkdir(mk_app_name)
@@ -147,21 +144,73 @@ app.config['SECRET_KEY'] = 'mysecretkey'"""
                     with open(mk_init_name, 'w') as f: f.write(init_file)
                     print(f'Created file at: {mk_init_name}')
                     mk_views_name = os.path.join(mk_app_name, views_name)
-                    with open(mk_views_name, 'w') as f: f.write("This is views.py")
+                    with open(mk_views_name, 'w') as f: f.write(views_file)
                     print(f"Created file at: {mk_views_name}")
                     mk_static_name = os.path.join(mk_app_name, static_directory_name)
                     os.mkdir(mk_static_name)
-                    print(f"Created direcotry at: {mk_static_name}")
+                    print(f"Created directory at: {mk_static_name}")
                     mk_templates_name = os.path.join(mk_app_name, template_directory_name)
                     os.mkdir(mk_templates_name)
                     print(f"Created directory at: {mk_templates_name}")
                     mk_stylesheet_name = os.path.join(mk_static_name, stylesheet_name)
-                    with open(mk_stylesheet_name, 'w') as f: f.write('This is styles.css')
+                    with open(mk_stylesheet_name, 'w') as f: f.write("""* {
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+}""")
                     print(f'Created file at: {mk_stylesheet_name}')
                     mk_template_name = os.path.join(mk_templates_name, index_name)
-                    with open(mk_template_name, 'w') as f:f.write("This is index.html")
+                    with open(mk_template_name, 'w') as f:f.write("""<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="{{url_for('static', filename='styles.css')}}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    
+</body>
+</html>""")
                     print(f'Created file at: {mk_template_name}')
-        elif os.path.isdir(path) == False:print(f"{path} is not a directory")
-        
+                    if blueprints != []:
+                        for x in blueprints:
+                            if x[-3:] != '.py': x+='.py'
+                            path = os.path.join(dir, webapp_name)
+                            path = os.path.join(path, webapp_name)
+                            path = os.path.join(path, x)
+                            f = open(path, 'w')
+                            if x[-3:] == '.py': x=x[:-3]
+                            f.write(f"""from flask import Blueprint, render_template, request, session, url_for
+{x} = Blueprint('{x}', __name__)""")
+                            f.close()
+                    if stylesheets != []:
+                        for x in stylesheets:
+                            if x[-4:] != '.css': x+='.css'
+                            path = os.path.join(dir, webapp_name)
+                            path = os.path.join(path, f'{webapp_name}/static')
+                            path = os.path.join(path, x)
+                            with open(path, 'w') as f:f.write("")
 
+                    if templates != []:
+                        for x in templates:
+                            path = os.path.join(dir, webapp_name)
+                            path = os.path.join(path, f'{webapp_name}/templates')
+                            path = os.path.join(path, x)
+                            with open(path, 'w') as f:f.write("""<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="{{url_for('static', filename='styles.css')}}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>""")
+
+        elif os.path.isdir(path) == False:print(f"{path} is not a directory")
 checkArgs(args)
